6a18ca0666372c1772f1ce4191f178ac4c5b3424
JuliaTsvigunenko <juliatsvigunenko5@gmail.com>
2023-02-07 20:31:06 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Задача 1
+
+            //int[] omas = new int[8];
+            //for ( int i=0; i<omas.Length; i++)
+            //{
+            //    Console.Write($"omas [{i}]=");
+            //    omas[i] = int.Parse(Console.ReadLine());
+            //}
+            //for (int i=0; i<omas.Length; i++)
+            //{
+            //    Console.WriteLine($"omas [{i}]={omas[i]}");
+            //}
+
+
+            // Задача 2
+
+            //double [] omas = { 5.6, 7, 8, -2, -7, 10, -6 };
+            //for ( int i=0; i<omas.Length; i++)
+            //{
+            //    if (omas[i]>0)
+            //    {
+            //        Console.WriteLine(omas[i]);
+            //    }
+            //}
+
+
+            // Задача 3
+
+            //int[] omas = { -7, 10, 5, 16, -4, 18 };
+            //Console.WriteLine("Вывод массива:");
+            //for ( int i = omas.Length-1; i>=0; i--)
+            //{
+            //    Console.WriteLine(omas[i]);
+            //}
+
+
+            // Задача 4
+
+            //int[] omas = new int[12];
+            //Random random = new Random();
+            //for (int i = 0; i <= 11; i++)
+            //{
+            //    omas[i] = random.Next(162, 191);
+            //    Console.WriteLine(omas[i]);
+            //}
+
+
+
+
+
+        }
+    }
+}
